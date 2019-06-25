e2966390a770638eeb947cb847ddc76216aafce9
gk <gk@GKN-7M21O1O74RR>
2019-06-25 10:19:44 +0200

    dete all

--- src/main/java/net/javaguides/springmvc/repository/ClientDao.java
+++
@@ -1,9 +0,0 @@
-package net.javaguides.springmvc.repository;
-
-import java.util.List;
-
-import net.javaguides.springmvc.entity.Client;
-
-public interface ClientDao {
-	public List<Client> getClientByName(String name);
-}
--- src/main/java/net/javaguides/springmvc/repository/CustomerDao.java
+++
@@ -1,9 +0,0 @@
-package net.javaguides.springmvc.repository;
-
-import java.util.List;
-
-import net.javaguides.springmvc.entity.Customer;
-
-public interface CustomerDao {
-	public List<Customer> getCustomerByName(String name);
-}
